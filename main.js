2052875f3baf634a927674ddef0a62d787582f10
Gilmore Davidson <gilmoreorless@gmail.com>
2018-10-26 12:57:32 +1100

    Rough SydCSS theme

--- main.js
+++ main.js
@@ -37,15 +37,19 @@ class Spinner {
 
 	setup(items) {
 		let container = document.createElement('div');
-		container.className = 'spinner-list-container';
+		container.className = 'spinner__list-container';
 
 		let listNode = this.listNode = document.createElement('ul');
-		listNode.className = 'spinner-list';
+		listNode.className = 'spinner__list';
 
 		this.setItems(items);
 
 		container.appendChild(listNode);
 		this.root.appendChild(container);
+
+		let overlayNode = document.createElement('div');
+		overlayNode.className = 'spinner__overlay';
+		this.root.appendChild(overlayNode);
 
 		// Must be called after the elements appear in the DOM, to correctly calculate height
 		this.transformItems();
@@ -57,7 +61,7 @@ class Spinner {
 		this.selectedIndex = 0;
 		for (let item of this.items) {
 			let itemNode = document.createElement('li');
-			itemNode.className = 'spinner-item';
+			itemNode.className = 'spinner__item';
 			itemNode.appendChild(document.createTextNode(item));
 			this.listNode.appendChild(itemNode);
 			this.itemNodes.push(itemNode);
@@ -74,7 +78,7 @@ class Spinner {
 		const radius = (itemHeight / 2) / Math.tan(itemAngle / 2);
 		this.itemNodes.forEach((node, i) => {
 			let angle = itemAngle * i;
-			node.style.transform = `translateY(-${itemHeight / 2}px) rotateX(-${angle}rad) translateZ(${radius}px)`;
+			node.style.transform = `translateY(-${itemHeight / 2}px) rotateX(-${angle}rad) translateZ(${radius - 1}px)`;
 		});
 	}
 
@@ -90,10 +94,9 @@ class Spinner {
 			const prop = angle => `rotateX(${angle}turn)`;
 			const duration = 5000 + Math.random() * 2000 - 1000;
 			
-	// 		this.listNode.style.setProperty('--rotation', `${minRotations + itemAngle}turn`);
 			let anim = this.listNode.animate(
 				{ 'transform': [prop(oldItemAngle), prop(turn)] },
-				{ duration, easing: 'ease-out', fill: 'forwards' }
+				{ duration, easing: 'cubic-bezier(.11,.69,.13,.98)', fill: 'forwards' }
 			);
 			anim.onfinish = () => resolve(anim);
 			this.selectedIndex = idx;
